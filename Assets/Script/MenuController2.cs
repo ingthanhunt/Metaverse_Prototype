3159126f4f3fc794ee12977ce12024e7ecbca429
worachot <wrcing21@gmail.com>
2022-06-08 18:09:37 +0700

    Add Number

---
+++ Assets/Script/MenuController2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartBtn2()
+    {
+        SceneManager.LoadScene("MINIGAME03");
+    }
+}
