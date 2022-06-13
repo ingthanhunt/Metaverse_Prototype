2320aa75bbbf59c5118e119bfee24a79694a7fce
worachot <wrcing21@gmail.com>
2022-06-13 19:08:10 +0700

    newMenuScene

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public Panel currentPanel = null;
+
+    private List<Panel> panelHistory = new List<Panel>();
+    
+    private void Start()
+    {
+        SetupPanels();
+    }
+
+    private void SetupPanels()
+    {
+        Panel[] panels = GetComponentsInChildren<Panel>();
+        
+        foreach ( Panel panel in panels)
+        {
+         panel.Setup(this);   
+        }
+
+        currentPanel.Show();
+    }
+
+    private void Update() {
+        
+    }
+
+    public void GoPrevious() {
+        if(panelHistory.Count == 0 ) {
+            return;
+        }
+
+        int  lastIndex = panelHistory.Count - 1;
+        SetCurrent(panelHistory[lastIndex]);
+        panelHistory.RemoveAt(lastIndex);
+    }
+
+    public void SetCurrentWithHistory(Panel newPanel) {
+        panelHistory.Add(currentPanel);
+        SetCurrent(newPanel);
+    }
+
+    public void SetCurrent(Panel newPanel) {
+        currentPanel.Hide();
+
+        currentPanel = newPanel;
+        currentPanel.Show();
+    }
+}
